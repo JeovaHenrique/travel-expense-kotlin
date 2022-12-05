770b7e0dc91c5f8439f55d45d6d23e6085abbe1e
Jeova Henrique <jeovahenrique92@gmail.com>
2022-12-05 15:42:45 -0300

    finish project add logic

--- app/src/main/java/com/exemplo/travelexpense/MainActivity.kt
+++ app/src/main/java/com/exemplo/travelexpense/MainActivity.kt
@@ -2,10 +2,49 @@ package com.exemplo.travelexpense
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.exemplo.travelexpense.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+       mBinding.buttonCalculate.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+        val id: Int = view.id
+        if (id == R.id.buttonCalculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        if (isValidationOk()) {
+
+            val distance = mBinding.editDistance.text.toString().toFloat()
+            val price = mBinding.editPrice.text.toString().toFloat()
+            val autonomy = mBinding.editAutonomy.text.toString().toFloat()
+
+            val total = (distance * price) / autonomy
+
+             mBinding.textResult.text = "R$ ${"%.2f".format(total)}"
+        } else {
+            Toast.makeText(this, getString(R.string.validation_fill_all_fields), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isValidationOk(): Boolean = (
+            mBinding.editDistance.text.toString() != "" &&
+                    mBinding.editPrice.text.toString() != "" &&
+                    mBinding.editAutonomy.text.toString() != "" &&
+                    mBinding.editAutonomy.text.toString() != "0"
+            )
+
 }
